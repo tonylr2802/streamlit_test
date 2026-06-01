d79d4fc51a479e06ccacef1d1753954898006ba8
Antonio Lopez <tonylr2802@gmail.com>
2026-06-01 11:02:38 -0600

    add px

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
 import streamlit as st
+import plotly.express as px
 
 # Leer los datos del archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
